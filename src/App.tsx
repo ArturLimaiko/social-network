5e94ede8ed21fa8a14a1cc02eff1b4182e8cc91c
Artur <alimayko91@gmail.com>
2024-04-24 21:18:32 +0300

    refactor code #1

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
-import {BrowserRouter,  Route} from "react-router-dom";
+import {BrowserRouter, Route} from "react-router-dom";
 import {News} from "./components/News/News";
 import {Music} from "./components/Music/Music";
 import {Settings} from "./components/Settings/Settings";
@@ -20,11 +20,11 @@ function App(props: any) {
                 <Navbar/>
                 <div className='app-wrapper-content'>
                     {/*Route следит за страницей например dialogs,profile и тд*/}
-                        <Route path='/dialogs' render= {()=> <Dialogs dialogs={ props.dialogs}/> }/>
-                        <Route path='/profile'  render= {()=> <Profile posts={props.posts} /> }/>
-                        <Route path='/news' component={News}/>
-                        <Route path='/music' component={Music}/>
-                        <Route path='/settings' component={Settings}/>
+                    <Route path='/dialogs' render={() => <Dialogs dialogs={props.dialogs} messages={props.message}/>}/>
+                    <Route path='/profile' render={() => <Profile posts={props.posts}/>}/>
+                    <Route path='/news' component={News}/>
+                    <Route path='/music' component={Music}/>
+                    <Route path='/settings' component={Settings}/>
                 </div>
             </div>
         </BrowserRouter>
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,24 +4,12 @@ import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
 
 
 
-export const Profile = () => {
-
-
-    let posts = [
-        {id: 1, message: 'Hi , how are you?', likesCount: 20},
-        {id: 2, message: 'Its my first post', likesCount: 54},
-        {id: 3, message: 'Its my first post', likesCount: 54},
-        {id: 4, message: 'Its my first post43', likesCount: 524},
-        {id: 5, message: 'Its my first post1', likesCount: 22},
-        {id: 6, message: 'Its my first post2', likesCount: 32},
-        {id: 7, message: 'Its my first post3', likesCount: 15},
-        {id: 8, message: 'Its my first post4', likesCount: 982},
-    ]
+export const Profile = (props:any) => {
 
     return (
         <div>
             <ProfileInfo/>
-            <MyPosts posts={posts}/>
+            <MyPosts posts={props.posts}/>
         </div>
     );
 };
