aecef3ddc53dfca17db43f3b02c31a6b27933be5
Artur <alimayko91@gmail.com>
2024-05-07 18:45:54 +0300

    lesson 37 complete #1

--- src/index.tsx
+++ src/index.tsx
@@ -12,11 +12,3 @@ let _callSubscriber = () => {
 _callSubscriber();
 
 store.subscribe(_callSubscriber);
-
-
-// переносим обратно в index.tsx все( export удалить)
-// стейт внутри параметров тоже удаляем, т.к стейт импортим из redux/state
-// в state tsx создадим функцию subscribe() {}  в нее передадим 1 параметр observer
-// subscribe импортим в index и внутрь него кладем rerenderEntireThree ( передаем колбек в стейт)
-// в state будем ее использовать
-//внутри subscribe функцию rerenderEntireThree = observer (приравниваем к функции)
