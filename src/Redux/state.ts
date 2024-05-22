c17366858672f9bf1b6bc7432ddbf4d31678ad95
Artur <alimayko91@gmail.com>
2024-05-22 13:20:07 +0300

    lesson 41 Complete Reducer

--- src/Redux/state.ts
+++ src/Redux/state.ts
@@ -132,6 +132,8 @@ export let store: StoreType = {
     }
 }
 
+
+
 //lesson 37 store-state
 // создаем переменную store и в нее кладем весь наш стейст state( state будет являться свойством),
 //  так же state делаем приватным _state
@@ -173,3 +175,20 @@ export let store: StoreType = {
 // onSendMessageChange функция которая которая принимает   event и внутри пишем let body = currentTarger.value
 // далее store.dispatch(updateNewMessageBodyCreator(body))
 // dispatch в функцию callBackAddMessage  sendMessageCreator()
+
+// lesson 41
+// action объект у которого есть свойство type
+// reducer чистая функция которая принимает state и action если нужно применяет этот action к этому state и возвращает новый new state либо возвращает не измененный state если он не подошел
+// создадим под каждый подобъект свой reducer
+// 1 создать функцию таким же именем  она принимает 2 параметра state & action и возвращает state
+// 2 переносим внутренности из state для каждого соответствующего action
+// 3 избавимся внутри от this и ProfilePage т.к state и есть тот ProfilePage(приходит к нам под именем state)
+//4  убираем callsubscriber() потому что это Reducer - его задача маленькая - получает state & action - преобразовывает и возвращает.
+//  либо может не преобразовать.
+// 5 export Reducer в state
+// 6 в dispatch передадим новые Reducer . их вызываем и отдаем часть - то есть то что ему нужно
+// например this._state.ProfilePage а вторым параметром action
+// 7 далее не забудем после преобразования веток стейта редьюсером, уведомить подписчика - в конце добавляем this._callSubscriber()
+// 8 преобразуем наши Reducer конструкцией switch(action.type) говорим по какому ключу мы переключаем(type) . и далее case {внутри сами кейсы например ADD-POST}
+// 9 после каждого кейса пропишет return state и return по умолчанию сделать  default : return state
+// 10 перенесем actionCreator к Reducer
